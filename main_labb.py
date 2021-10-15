0f5240c4639d1c85a0ae0265b969744f890dac10
chasvibe <viktor.berg@chasacademy.se>
2021-10-15 12:06:24 +0200

    app done

--- main_labb.py
+++ main_labb.py
@@ -1,54 +1,17 @@
 import random
-from betting import betting
 
 
-def winnings(total_bet, guesses):
-    return total_bet * guesses
+# Takes users bet and multiplies it with the amount of guesses the user has left
+def winnings(user_bet, guesses):
+    return user_bet * guesses
 
 
-def game(correct_answer):
-
-    counter = 10 # Counter that goes to 10 and then quits, if right answer: resets
-    new_bet = 0
-    bet = eval(input("How much would you like to bet?:\n"))  # The amount the user wants to bet
-    while counter >= 0:
-        try:
-            guess = eval(input("Please enter a guess:\n"))
-            new_bet += bet
-            if counter == 0:
-                print("Sorry your guesses ran out. . .")
-                break
-            elif guess == 'quit()':
-                break
-            elif guess == correct_answer:
-                print("""
-                Congratulations! You guessed right!
-                
-                """)
-                winnings(new_bet, counter)
-                break
-            elif guess < 0 or guess > 40:
-                print("Please try guessing a whole number between '1' and '40'")
-            elif guess < correct_answer:
-                print("Sorry, too low. . .")
-                print(f"You have {counter} guesses left. . .")
-                counter -= 1
-            elif guess > correct_answer:
-                print("Sorry, too high. . .")
-                print(f"You have {counter} guesses left. . .")
-                counter -= 1
-        except NameError:
-            print("Please try guessing a whole number between '1' and '40'")
-        except TypeError:
-            print("Please try guessing a whole number between '1' and '40'")
-        except KeyboardInterrupt:
-            print("Please try guessing a whole number between '1' and '40'")
-        except SyntaxError:
-            print("Please try guessing a whole number between '1' and '40'")
+# Takes the deposition minus bet, and returns what's left
+def bet(user_deposition, user_bet):
+    return user_deposition - user_bet
 
 
-random_number = random.randint(1, 40)  # Correct answer
-deposition = eval(input("How much do you want to deposit?:\n"))  # Amount of money the user inputs
+
 print("""
                             Time to guess!
            Your job is to guess a number between '1' and '40'
@@ -57,10 +20,89 @@ print("""
           the total amount that you bet so far will be multiplied
                  by the amount of guesses you have left.  
             """)
-print(f"Your balance is: {deposition}")
-game(random_number)
-#new_input = str(input("Would you like to play again?\n\t [Y], [N]"))
-#if new_input.upper() == 'Y':
-#    game(random_number)
-#else:
+counter = 10  # Counter that goes to 10 and then quits, if right answer: resets
+loop_counter = 0
+while True:
+    try:
+        deposition = eval(input("\t\t    How much do you want to deposit?:\n\t\t\t\t  "))  # Amount of money the user inputs
+        if deposition < 100:
+            print("\t\t    Please enter a value over 100. . .\n ")
+        else:
+            break
+    except NameError:
+        print("\t\t    Please enter a value over 100. . .\n ")
+    except TypeError:
+        print("\t\t    Please enter a value over 100. . .\n ")
+    except KeyboardInterrupt:
+        print("\t\t    Please enter a value over 100. . .\n ")
+    except SyntaxError:
+        print("\t\t    Please enter a value over 100. . .\n ")
+
+
+print(f"\t\t\t¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤")
+print(f"\t\t\t  Your balance is: {deposition}")
+print(f"\t\t\t¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤")
+
+
+while deposition > 0:
+    try:
+        random_number = random.randint(1, 40)  # Correct answer
+        while True:
+            user_bet = eval(input("\t\t     How much would you like to bet?:\n\t\t\t\t  "))  # The amount the user wants to bet
+            if user_bet > deposition:
+                print("\t\t   You cannot bet more than you own. . .")
+            else:
+                break
+        new_user_deposition = bet(deposition, user_bet)
+        while counter >= 0:
+            try:
+                guess = eval(input("\t\t\t  Please enter a guess:\n\t\t\t\t  "))
+                if counter == 0:
+                    print("\t\t\t  Sorry your guesses ran out. . .")
+                    new_user_deposition -= user_bet
+                    deposition = new_user_deposition
+                    break
+                elif guess == 'quit()':
+                    break
+                elif guess == random_number:
+                    print("""
+                   ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+                   Congratulations! You guessed right!
+                   ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+                    """)
+                    new_user_deposition += winnings(user_bet, counter)
+                    counter = 10
+                    deposition = new_user_deposition
+                    print(f"\t\t\t¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤")
+                    print(f"\t\t\t  Your balance is: {new_user_deposition}")
+                    print(f"\t\t\t¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤")
+                    break
+                elif guess < 0 or guess > 40:
+                    print("\tPlease try guessing a whole number between '1' and '40'")
+                elif guess < random_number:
+                    print("\t\t\t    Sorry, too low. . .")
+                    print(f"\t\t\tYou have {counter} guesses left. . .")
+                    counter -= 1
+                elif guess > random_number:
+                    print("\t\t\t    Sorry, too high. . .")
+                    print(f"\t\t\tYou have {counter} guesses left. . .")
+                    counter -= 1
+            except NameError:
+                print("\tPlease try guessing a whole number between '1' and '40'")
+            except TypeError:
+                print("\tPlease try guessing a whole number between '1' and '40'")
+            except KeyboardInterrupt:
+                print("\tPlease try guessing a whole number between '1' and '40'")
+            except SyntaxError:
+                print("\tPlease try guessing a whole number between '1' and '40'")
+    except NameError:
+        print("\tPlease try guessing a whole number between '1' and '40'")
+    except TypeError:
+        print("\tPlease try guessing a whole number between '1' and '40'")
+    except KeyboardInterrupt:
+        print("\tPlease try guessing a whole number between '1' and '40'")
+    except SyntaxError:
+        print("\tPlease try guessing a whole number between '1' and '40'")
+
+
 print("Thank you for playing! Come back soon!")
