af7b7230ccb86dad502515673c0e7272c5c30939
chasvibe <viktor.berg@chasacademy.se>
2021-10-22 10:18:40 +0200

    bash update

--- bash/append.sh
+++ bash/append.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "import lapingold" > main.py
-echo "import vibe" > main.py
-echo "" > main.py
-cat lapingold.py > main.py
-echo "" > main.py
-echo "" > main.py
-cat vibe.py > main.py
+> main.py
+cat lapingold.py >> main.py
+cat vibe.py >> main.py
---
+++ bash/main.py
@@ -0,0 +1,8 @@
+def multiply(number1, number2):
+    return number1 * number2
+
+
+def add(number1, number2):
+    return number1 + number2
+
+
