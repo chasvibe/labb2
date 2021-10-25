495004ef846a353a66e421d1cf47fa710f79fbb3
chasvibe <viktor.berg@chasacademy.se>
2021-10-25 14:03:46 +0000

    restoring files

---
+++ bash/append.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+> main.py
+cat lapingold.py >> main.py
+cat vibe.py >> main.py
---
+++ razm.py
@@ -0,0 +1,10 @@
+a = int(input("Input your first number: "))
+b = int(input("Input your seconde number: "))
+
+
+def upphojtill(a,b):
+    theAnswer = 1
+    for i in range(b):
+        theAnswer = theAnswer * a
+        i = i +1
+    return theAnswer
