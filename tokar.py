862218bd21bd06de1c83fa56a285948e8d71aecb
tompakarlsson <tompa.karlsson@gmail.com>
2021-10-26 14:40:17 +0200

    changed

--- tokar.py
+++ tokar.py
@@ -4,8 +4,10 @@ def tokar(word: str):
     Returns: string that tells if word is a palindrome
     Example:
     tokar("Dallassallad")'''
-    stripped_word = word.replace('.', '').replace(' ', ''.replace(',', '')
+    stripped_word = word.replace('.', '').replace(' ', '').replace(',', '')
     if stripped_word.lower() == stripped_word[::-1].lower():
         return f'{word} is a palindrome'
     else:
         return f'{word} is not a palindrome'
+
+print(tokar("Mus rev inuits öra, sa röst i universum"))
