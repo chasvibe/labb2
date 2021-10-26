0f46ef597513a4729767be2f26d211ecc01fdc8e
tompakarlsson <tompa.karlsson@gmail.com>
2021-10-26 14:54:49 +0200

    added

---
+++ tokar.py
@@ -0,0 +1,11 @@
+def tokar(word: str):
+    '''Check if a string is a palindrome
+    Parameters: word
+    Returns: string that tells if word is a palindrome
+    Example:
+    tokar("Dallassallad")'''
+    stripped_word = word.replace('.', '').replace(' ', '').replace(',', '')
+    if stripped_word.lower() == stripped_word[::-1].lower():
+        return f'"{word}" is a palindrome'
+    else:
+        return f'"{word}" is not a palindrome'
