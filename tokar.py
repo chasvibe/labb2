8119cd052d4440ba2ff51c53d2508eaba9b3e0a3
tompakarlsson <tompa.karlsson@gmail.com>
2021-10-26 14:41:44 +0200

    changed

--- tokar.py
+++ tokar.py
@@ -6,8 +6,8 @@ def tokar(word: str):
     tokar("Dallassallad")'''
     stripped_word = word.replace('.', '').replace(' ', '').replace(',', '')
     if stripped_word.lower() == stripped_word[::-1].lower():
-        return f'{word} is a palindrome'
+        return f'"{word}"" is a palindrome'
     else:
-        return f'{word} is not a palindrome'
+        return f'"{word}"" is not a palindrome'
 
 print(tokar("Mus rev inuits öra, sa röst i universum"))
